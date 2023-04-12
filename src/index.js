19597bba603d05cfd2659cb07693e20fa9863656
dan <dancr.wley@gmail.com>
2023-04-12 16:45:29 -0400

    added task tracker functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+
+
+
+const toDoForm = document.querySelector('form')
+  toDoForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleInput(e.target[0].value)
+    toDoForm.reset()
+  })
 });
+
+function handleInput(todo) {
+  let p = document.createElement('p')
+  p.textContent = `${todo}`
+  let btn = document.createElement('button')
+    btn.addEventListener('click', deleteBtn)
+    btn.textContent = 'X'
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
+}
+
+
+
+
+function deleteBtn(e) {
+  e.target.parentNode.remove()
+}
